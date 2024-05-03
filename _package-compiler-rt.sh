20f025883046a09d1a7a55d10f71c7534961a4ae
Rasmus Andersson <rasmus@notion.se>
2024-05-03 12:18:30 -0700

    fixes issue with macos targets where compiler-rt builtins would not be found

--- _package-compiler-rt.sh
+++ _package-compiler-rt.sh
@@ -8,13 +8,17 @@ fi
 
 case $TARGET in
   *macos*)
-    # Clang's darwin toolchain impl assumes rt libs are at ResourceDir/lib/darwin/.
-    # See MachO::AddLinkRuntimeLib in clang/lib/Driver/ToolChains/Darwin.cpp
-    _cpmerge $BUILTINS_DIR/lib    lib/clang/lib/darwin
-    _cpmerge $COMPILER_RT_DIR/lib lib/clang/lib/darwin
+    # Clang's darwin toolchain has a complicated filename scheme for rt libs:
+    # - treats builtins specially by not appending its name,
+    #   i.e. libclang_rt.osx.a not libclang_rt.builtins_osx.a
+    # - appends "_osx" to library names, but appends "osx" to builtins
+    # - appends "_dynamic" to shared libraries
+    mkdir -p lib/clang/lib
+    cp -v $BUILTINS_DIR/lib/libclang_rt.builtins.a lib/clang/lib/libclang_rt.osx.a
+    _cpmerge $COMPILER_RT_DIR/lib                  lib/clang/lib
     ;;
   wasm*)
-    _cpmerge $BUILTINS_DIR/lib    lib/clang/lib
+    _cpmerge $BUILTINS_DIR/lib lib/clang/lib
     ;;
   *linux|*playbit)
     _cpmerge $BUILTINS_DIR/lib    lib/clang/lib
